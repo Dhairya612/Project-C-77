9270c3c0a268e7e77aab3e22f71b25709dae7a38
Dhairya612 <76558713+Dhairya612@users.noreply.github.com>
2021-04-26 14:46:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name="Dhairya";
+            console.log(first_name);
+            var last_name="Shah";
+            console.log (last_name);
+       var full_name= first_name.concat(last_name);
+       console.log(4);
+       console.log(15);
+       console.log(10);
